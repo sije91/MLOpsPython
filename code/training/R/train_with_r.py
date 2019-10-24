08f6a131f89e6e4e0abe3c88e4bf6d14bccf41e2
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-10-24 10:58:22 -0700

    Training with R

--- code/training/R/train_with_r.py
+++ code/training/R/train_with_r.py
@@ -1,3 +1,18 @@
 import os
+from azureml.core.run import Run
+
+run = Run.get_context()
 
 os.system("Rscript r_train.r && ls -ltr model.rds")
+
+# upload the model file explicitly into artifacts
+model_name = "model.rds"
+run.upload_file(name="./outputs/" + model_name, path_or_stream=model_name)
+print("Uploaded the model {} to experiment {}".format(
+    model_name, run.experiment.name))
+#dirpath = os.getcwd()
+##print(dirpath)
+print("Following files are uploaded ")
+print(run.get_file_names())
+
+run.complete()
